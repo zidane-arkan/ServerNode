9b2471b4be97949aced07d4fd33162e4bc7c8d99
zidane-arkan <zidark897@Gmail.com>
2022-10-07 07:13:23 +0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Listening For request on Port 3000');
+});
