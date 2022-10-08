66ac7f3a45de2d69e6109653611a8a5259949b4d
zidane-arkan <zidark897@Gmail.com>
2022-10-08 20:18:49 +0700

    Update about and index, app

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+//Express App
+const app = express();
+let path = './views';
+
+//Listen For Request
+app.listen(3000);
+
+//Response Request
+app.get('/', (req, res) => {
+    //Menggantikan res.setHeader
+    // res.send('<h1>Halo Semua</h1>');
+    res.sendFile(`./views/index.html`, { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(`./views/about.html`, { root: __dirname });
+});
+
+//Redirecting
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+//404 Page
+//Middleware
+app.use((req, res) => { 
+    res.sendFile('./views/404.html',{root : __dirname});
+});
