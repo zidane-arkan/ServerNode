9ce425ef02a4269ebd41f7c7450b318a7b720dea
zidane-arkan <zidark897@Gmail.com>
2022-10-09 08:51:43 +0700

    Add views and fix bug 1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 // let path = './views';
 
 //Register View Engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //Listen For Request
 app.listen(3000);
@@ -13,21 +13,25 @@ app.listen(3000);
 //Response Request
 app.get('/', (req, res) => {
     //Menggantikan res.setHeader
-    // res.send('<h1>Halo Semua</h1>');
-    res.sendFile(`./views/index.html`, { root: __dirname });
+    // res.sendFile(`./views/index.ejs`, { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(`./views/about.html`, { root: __dirname });
+    res.render('about');
+});
+
+app.get('/blogs/create', (req, res) => { 
+    res.render('create');
 });
 
 //Redirecting
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
 
 //404 Page
 //Middleware
-app.use((req, res) => { 
-    res.sendFile('./views/404.html',{root : __dirname});
+app.use((req, res) => {
+    res.status(404).render('404');
 });
