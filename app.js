4483cd9bb32cef64fc7855c87b9224bf3ea261c7
zidane-arkan <zidark897@Gmail.com>
2022-10-09 10:27:08 +0700

    Add Blogs content dummy data

--- app.js
+++ app.js
@@ -12,17 +12,22 @@ app.listen(3000);
 
 //Response Request
 app.get('/', (req, res) => {
+    const blogData = [
+        { title: 'Lupa sunat saat makan', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+        { title: 'Meninjau proses minum', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+        { title: 'Kenapa anda gila ?', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
+    ];
     //Menggantikan res.setHeader
     // res.sendFile(`./views/index.ejs`, { root: __dirname });
-    res.render('index');
+    res.render('index', { title: 'Home', blogData });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about',{title : 'About'});
 });
 
 app.get('/blogs/create', (req, res) => { 
-    res.render('create');
+    res.render('create', {title : "Create New Blog"});
 });
 
 //Redirecting
