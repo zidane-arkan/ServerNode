ae4bd6dbac985ddc59ee5e69e445701bfe24e7df
zidane-arkan <zidark897@Gmail.com>
2022-10-10 11:24:18 +0700

    Change Routes and Add Data to index

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const Blog = require('./models/blog');
 //Express App
 const app = express();
 
@@ -33,21 +33,32 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-
-//Response Request
+//Routes
 app.get('/', (req, res) => {
-    const blogData = [
-        { title: 'Lupa sunat saat makan', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-        { title: 'Meninjau proses minum', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-        { title: 'Kenapa anda gila ?', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
-    ];
+    // const blogData = [
+    //     { title: 'Lupa sunat saat makan', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    //     { title: 'Meninjau proses minum', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    //     { title: 'Kenapa anda gila ?', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
+    // ];
     //Menggantikan res.setHeader
     // res.sendFile(`./views/index.ejs`, { root: __dirname });
-    res.render('index', { title: 'Home', blogData });
+    // res.render('index', { title: 'Home', blogData });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
+});
+
+//Blog Routes
+app.get('/blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.render('index', { title: 'Home', blogData: result });
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 });
 
 app.get('/blogs/create', (req, res) => {
@@ -63,3 +74,43 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404');
 });
+
+//Mongoose and Mongo Sandbox Routes
+//Save Data to Db
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'New Blog Hey',
+//         content: 'Lorem ipsum adhasadha dadhsadhasdhahsa',
+//         bodyContent: 'Lorem asdsaads sada da das das das dasd a'
+//     });
+//     blog
+//         .save()
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             res.send(err);
+//         });
+//     res.redirect('/blogs/create');
+// });
+//Get All Data From Db
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             res.send(err);
+//         });
+// });
+//Get Single Data From Db By Id
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('63439b296cf13019cc9d6c66')
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             res.send(err);
+//         });
+// });
+
