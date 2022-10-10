acfa113d2868721e23f16b8a2c764b3dd4502767
zidane-arkan <zidark897@Gmail.com>
2022-10-10 11:30:55 +0700

    Show blog data to index views

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get('/about', (req, res) => {
 
 //Blog Routes
 app.get('/blogs', (req, res) => {
-    Blog.find()
+    Blog.find().sort({createdAt: -1})
         .then((result) => {
             res.render('index', { title: 'Home', blogData: result });
         })
